ed4b686d9df9f9350a761fba8e6226eb2103ac1f
Tobby_000 <q1142741687@163.com>
2026-03-24 22:33:07 +0800

    fix:补充遗漏的地址获取

--- middleware/currentPath.js
+++ middleware/currentPath.js
@@ -1,7 +1,8 @@
 // middleware/currentPath.js
+const config = require("../config");
 module.exports = function (req, res, next) {
   // 将当前路径注入到res.locals，所有视图都能访问
-  res.locals.currentPath = req.path;
+  res.locals.currentPath = config.normalizedApp;
   res.locals.isHomePage = req.path === "/";
   res.locals.isAboutPage = req.path.startsWith("/about");
   next();
